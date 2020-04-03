a0e6fdbd02a4298dcd342fd9e1f33e778a8d555a
Muthahir <60794327+muthahirb@users.noreply.github.com>
2020-04-03 16:15:19 +0200

    Final_delay _coe_matlab code
    this code has the delay with hex values with positive integers in it and also creates a .coe file

--- matlab_convert_coefile.m
+++ matlab_convert_coefile.m
@@ -1,21 +1,17 @@
-%%dalay data is converted to coe.file
-%the order of data saving is put the first column(the first micro) to the
-%second column(the second micro)and so on. The first 180 datas can be assigned to the first micro, 
-%181th data to 360th data can be assigned to the seond micro and so on.
-%%
-clc;
-clear;
-width=8;% the width of data saved in ROM
-%depth=256;%the depth of data saved in ROM(the number of the data of delays)
-y=delays;%dalays will be saved in ROM
-fid=fopen('test_data.coe','w');%open a .coe file
-%store the first line as hexadecimal number
-fprintf(fid,'memory_initialization_radix=16;\n');
-%store the second line as hexadecimal number
-fprintf(fid,'memory_initialization_vector=\n');
-%store the 4343th data
-fprintf(fid,'%x,\n',y(1:end-1));
-%store the last data
-fprintf(fid,'%x;\n',y(end));
-fclose(fid);%close the coe.file
-%not sure if the data is saved as hexadecimal number???????
+d = 11.4e-3;
+c = 343;
+A=zeros(150,24);
+Z=zeros(150,24);
+width=16;
+fid=fopen('test_data.coe','w');
+%fprintf(fid,'memory_initialization_radix=16;\n');
+
+for i=0:1:6
+   k=30+20*i;
+    for j=1:1:24
+           A(k,j)=round(((j-1)*d*cosd(k))*10^6/c);
+           Z=abs(A(k,j));
+           fprintf(fid,'%x,\n',Z);
+    end
+end
+
