8ed1907f57495ea1dbecbd7e2b1397e0dc24e7cc
Muthahir <60794327+muthahirb@users.noreply.github.com>
2020-05-09 18:19:13 +0200

    Update delay_program.m

--- delay_program.m
+++ delay_program.m
@@ -10,7 +10,14 @@ fid=fopen('test_data.coe','w');
 for i=0:1:6
    k=30+20*i;
     for j=1:1:24
-           A(k,j)=round(((j-1)*d*cosd(k))*1.41*10^6/c); % 1.41 is the PDM clock cycle
+        if k > 90 % if condition to invert the data after 90degree
+            
+            A(k,25-j)=round(((j-1)*d*cosd(k))*1.41e6/c); % 1.41 is the PDM clock cycle
+        else
+            A(k,j)=round(((j-1)*d*cosd(k))*1.41e6/c); % 1.41 is the PDM clock cycle
+        end
+        
+         %  A(k,j)=round(((j-1)*d*cosd(k))*1.41e6/c); % 1.41 is the PDM clock cycle
 %            Delay_real_time=round(((j-1)*d*cosd(k))/c);
 %            pdm_clock;
 %            clock_cycle_lengh = 1/pdm_clock;
@@ -20,4 +27,5 @@ for i=0:1:6
            fprintf(fid,'%x,\n',Z);
     end
 end
-
+mamimum_delay= max(B);
+Maximum_clock= max(A);
